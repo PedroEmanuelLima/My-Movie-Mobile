cfe6a0dd107d6917788a36dc5f442eabee03ec7c
Pedro Emanuel <58365600+PedroEmanuelLima@users.noreply.github.com>
2022-04-26 22:46:51 -0300

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -12,7 +12,7 @@ export const api = axios.create({
     baseURL: `https://api.themoviedb.org/`,
     https: config,
     params: {
-        api_key: '129304453114a295f6fbbf9d007ecdb1',
+        api_key: '',
     },
 
 });
